453138d5c7c397a2187815bf78e76bc66cb6cf95
Alex Billson <alex.billson@sky.uk>
2021-08-21 12:37:13 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Alex Billson on 21/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

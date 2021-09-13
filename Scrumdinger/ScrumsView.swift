a8a7cd357a7f4886628502e4f13c3faf0f74c6a9
Alex Billson <alex.billson@sky.uk>
2021-09-13 16:10:38 +0100

    Creating a Navigation Hierarchy

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,36 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Alex Billson on 01/09/2021.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                NavigationLink(
+                    destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.color)
+            }
+        }
+        .navigationTitle("Daily Scrums")
+        .navigationBarItems(trailing: Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+            Image(systemName: "plus")
+        })
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ScrumsView(scrums: DailyScrum.data)
+        }
+    }
+}

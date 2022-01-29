1e6d88b218ec0d3dd371e9dfdb9e06c4863de8e1
Alex Billson <alex.billson@sky.uk>
2022-01-29 15:44:32 +0000

    Create an Overlay view

---
+++ Scrumdinger/MeetingHeaderView.swift
@@ -0,0 +1,47 @@
+//
+//  MeetingHeaderView.swift
+//  Scrumdinger
+//
+//  Created by Alex Billson on 29/01/2022.
+//
+
+import SwiftUI
+
+struct MeetingHeaderView : View {
+    let secondsElapsed: Int
+    let secondsRemaining: Int
+    
+    private var totalSeconds: Int {
+        secondsElapsed + secondsRemaining
+    }
+    
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("\(secondsElapsed)", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+        }
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel(Text("Time Remaining"))
+        .accessibilityLabel(Text("10 minutes"))
+    }
+}
+
+struct MeetingHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180)
+            .previewLayout(.sizeThatFits)
+    }
+}

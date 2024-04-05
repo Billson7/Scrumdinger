e049b6df879347b8acc12595b3f4237927a0cd78
Alex Billson <alex.billson@sky.uk>
2024-04-05 15:54:58 +0100

    Create an error view

---
+++ Scrumdinger/Models/ErrorWrapper.swift
@@ -0,0 +1,8 @@
+//
+//  ErrorWrapper.swift
+//  Scrumdinger
+//
+//  Created by Alex Billson on 05/04/2024.
+//
+
+import Foundation
---
+++ Scrumdinger/Views/ErrorView.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorView.swift
+//  Scrumdinger
+//
+//  Created by Alex Billson on 05/04/2024.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ErrorView()
+}

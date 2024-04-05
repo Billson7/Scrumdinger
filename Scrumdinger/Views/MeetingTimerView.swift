b1242143e1762e50750a1b242d801aa294190519
Alex Billson <alex.billson@sky.uk>
2024-04-05 16:28:45 +0100

    initial commit for drawing timer view

---
+++ Scrumdinger/Views/MeetingTimerView.swift
@@ -0,0 +1,20 @@
+//
+//  MeetingTimerView.swift
+//  Scrumdinger
+//
+//  Created by Alex Billson on 05/04/2024.
+//
+
+import SwiftUI
+
+struct MeetingTimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeetingTimerView()
+}
+
+//https://developer.apple.com/tutorials/app-dev-training/drawing-the-timer-view

da1564e93b941dae172b97f2bd78aabf46d417c4
Alex Billson <alex.billson@sky.uk>
2024-04-19 13:36:50 +0100

    initial  commit

--- Scrumdinger/Views/MeetingTimerView.swift
+++ Scrumdinger/Views/MeetingTimerView.swift
@@ -9,12 +9,10 @@ import SwiftUI
 
 struct MeetingTimerView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
     }
 }
 
 #Preview {
     MeetingTimerView()
 }
-
-// https://developer.apple.com/tutorials/app-dev-training/drawing-the-timer-view

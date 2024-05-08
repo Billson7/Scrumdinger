6c5e07aca4b86c9607305472a54427cf9e8a7cf7
Alex Billson <alex.billson@sky.uk>
2024-05-08 17:13:09 +0100

    correct a varaible typo

--- Scrumdinger/Views/SpeakerArc.swift
+++ Scrumdinger/Views/SpeakerArc.swift
@@ -25,7 +25,7 @@ struct SpeakerArc: Shape {
     
     func path(in rect: CGRect) -> Path {
         let diameter = min(rect.size.width, rect.size.height) - 24.0
-        let radius = diamter / 2.0
+        let radius = diameter / 2.0
         let center = CGPoint(x: rect.midX, y: rect.midY)
         return Path { path in
             path.addArc(center: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
